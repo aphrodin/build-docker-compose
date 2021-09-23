3ae0ede235d23900de81076ee8f64b0f6594ed6f
amoudnib <anas.moudnibe@gfi.world>
2021-09-23 15:11:50 +0100

    Initial commit

--- polling-app-server/src/test/java/com/example/polls/PollsApplicationTests.java
+++ polling-app-server/src/test/java/com/example/polls/PollsApplicationTests.java
@@ -4,13 +4,30 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import com.example.polls.model.RoleName;
+import com.example.polls.model.Role;
+import com.example.polls.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PollsApplicationTests {
 
+	@Autowired
+    private RoleRepository roleRepository;
+
 	@Test
 	public void contextLoads() {
 	}
+
+	@Test
+ 	public void findusers() throws Exception {
+
+		roleRepository.findByName(RoleName.ROLE_USER);
+
+
+}
+
 
 }
